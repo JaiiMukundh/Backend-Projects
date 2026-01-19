9f26b49dea940e3cf8708fdfcfc5361ed177acfa
JaiiMukundh <ajaiimukundh@gmail.com>
2026-01-19 10:17:00 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const {DB_NAME} = require('./constants');
-const connectDB = require('./db/index');
-
-const app = express();
-
-connectDB();
